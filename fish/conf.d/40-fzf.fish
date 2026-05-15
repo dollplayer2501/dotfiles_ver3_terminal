a9f091e8913427c93bf8d5a09ebacc98582166d5
Masahiquo, Naraya MORRY <kerberos.gm@gmail.com>
2026-05-15 10:41:16 +0900

    little fix, fzf of fish shell

--- fish/conf.d/40-fzf.fish
+++ fish/conf.d/40-fzf.fish
@@ -16,16 +16,23 @@
 
 # vitormv.github.io fzf Theme Generator
 # https://vitormv.github.io/fzf-themes/
+
+#
+# NOTE:
+#  - Shift + Up/Down is enabled in right preview pane
+#
+
 set --export FZF_DEFAULT_OPTS '
   --height=35%
   --layout=reverse
   --border
   --inline-info
-  --preview "bat --color=always {}"
-  --color=fg:#d0d0d0,fg+:#d0d0d0
-  --color=hl:#5f87af,hl+:#5fd7ff,info:#afaf87,marker:#87ff00
-  --color=prompt:#d7005f,spinner:#af5fff,pointer:#af5fff,header:#87afaf
-  --color=border:#7f3fbf,label:#aeaeae,query:#d9d9d9
+  --preview "bat --theme='neofusion' --style=numbers --color=always {}"
+  --bind "shift-down:preview-down,shift-up:preview-up"
+  --color=fg:#9999cc,fg+:#ff7f7f
+  --color=hl:#5f87af,hl+:#5fd7ff,info:#afaf87,marker:#ff7f7f
+  --color=prompt:#af5fff,spinner:#af5fff,pointer:#ff7f7f,header:#87afaf
+  --color=border:#7f3fbf,label:#7f3fbf,query:#ff7f7f
   --border="rounded" --border-label="FZF" --preview-window="border-rounded" --prompt="> "
   --marker=">" --pointer="◆" --separator="─" --scrollbar="│"'
 
