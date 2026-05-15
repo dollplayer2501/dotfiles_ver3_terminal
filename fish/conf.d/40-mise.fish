e18a8104ba495ae4b9c723994216b01a66fa806a
Masahiquo, Naraya MORRY <kerberos.gm@gmail.com>
2026-05-15 10:49:21 +0900

    little fix, mise:mise-en-place at fish shell, but it does not include completions

---
+++ fish/conf.d/40-mise.fish
@@ -0,0 +1,14 @@
+#
+# mise-en-place
+#  https://mise.jdx.dev/
+#  https://github.com/jdx/mise
+#
+
+/usr/bin/mise activate fish | source
+
+#
+# NOTE:
+# See https://mise.jdx.dev/installing-mise.html
+# Only first, after install:
+# > mise completion fish > ~/.config/fish/completions/mise.fish
+#
