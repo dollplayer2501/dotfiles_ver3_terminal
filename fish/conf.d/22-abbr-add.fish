035bf47e6b81839f44f42c5e3d4c301823300738
Masahiquo, Naraya MORRY <kerberos.gm@gmail.com>
2026-07-05 17:44:01 +0900

    fix fish shell, abbr --add

--- fish/conf.d/22-abbr-add.fish
+++ fish/conf.d/22-abbr-add.fish
@@ -13,10 +13,17 @@ abbr --add bat 'command bat --theme="neofusion"'
 abbr --add delta 'command delta --side-by-side --line-numbers'
 abbr --add feh 'command feh --no-fehbg --bg-fill'
 abbr --add pict-identify 'command identify -format "%wx%h:%Q"'
-abbr --add yay 'yay --sudoloop'
 
-# abbr --add cd z # zoxide
+#
+
+abbr --add yay 'yay --sudoloop'
+abbr --add update-package-non-aur 'sudo pacman -Syu'
+abbr --add update-package-aur 'yay -Sua --aur --editmenu'
+
+#
 
 abbr --add la 'command lsd --long --almost-all --classify --versionsort --color=auto --date "+%Y-%m-%d %H:%M"'
 abbr --add my_fastfetch_full 'command fastfetch --config ~/.config/fastfetch/config..full.jsonc'
 abbr --add my_fastfetch_short 'command fastfetch --config ~/.config/fastfetch/config..short.jsonc'
+
+# abbr --add cd z # zoxide
