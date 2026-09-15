8252a53d45394fe58ab8a19c4a389eab7cee4d9f
Masahiquo, Naraya MORRY <kerberos.gm@gmail.com>
2026-09-15 16:17:45 +0900

    THIS IS TEST, HOOKS,PRE-COMMIT

--- _bin/my_backupping5.fish
+++ _bin/my_backupping5.fish
@@ -1,9 +1,5 @@
 #!/usr/bin/env fish
 
-# set --local script_dir (dirname (status --current-filename))
-# echo $script_dir
-
-# set --local ini_file /home/dollplayer/Projects/dotfiles_ver3_terminal/_bin/my_backupping5.ini
 set --local ini_file ~/.local/bin/my_backupping5.ini
 __my_function_load_ini "$ini_file"
 if test $status -ne 0
@@ -12,7 +8,12 @@ end
 # echo $backup_path_public
 # echo $backup_path_hidden
 
+
 echo (date) > $backup_path_public/hoge_public.txt
 echo (date) > $backup_path_hidden/hoge_hidden.txt
-
 git add $backup_path_public/hoge_public.txt
+
+
+command fc-list : family | sort -u | grep -v "Noto" > $backup_path_public/fc-list.txt
+git add $backup_path_public/fc-list.txt
+
